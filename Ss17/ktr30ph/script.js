a8b574338e84049e11b27b237473cdd26209b06f
Qtrung474 <biunon83@gmail.com>
2026-03-17 21:46:37 +0700

    Add files via upload

---
+++ Ss17/ktr30ph/script.js
@@ -0,0 +1,13 @@
+let products = [
+  { code: "SP001", name: "Laptop Dell XPS 15", price: 25000000 },
+  { code: "SP002", name: "iPhone 15 Pro Max", price: 32900000 },
+  { code: "SP003", name: "Samsung Galaxy S24 Ultra", price: 28900000 },
+  { code: "SP004", name: "Tai nghe AirPods Pro 2", price: 5990000 },
+  { code: "SP005", name: "Bàn phím cơ Logitech MX Keys", price: 2990000 },
+  { code: "SP006", name: "Chuột không dây Logitech MX Master 3", price: 2490000 },
+  { code: "SP007", name: "Màn hình Dell UltraSharp U2720Q", price: 12900000 },
+  { code: "SP008", name: "MacBook Pro 14 inch M3", price: 45900000 },
+  { code: "SP009", name: "iPad Pro 12.9 inch M2", price: 28900000 },
+  { code: "SP010", name: "Loa Bluetooth JBL Flip 6", price: 3990000 }
+];
+
